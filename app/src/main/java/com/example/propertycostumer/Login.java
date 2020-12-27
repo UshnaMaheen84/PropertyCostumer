a05587c2ce503e25a5e8cde8b47094a9e2836a0b
Rimsha84 <rimsha732@gmail.com>
2020-12-27 14:49:55 +0500

    initial commit

--- app/src/main/java/com/example/propertycostumer/Login.java
+++ app/src/main/java/com/example/propertycostumer/Login.java
@@ -1,14 +1,144 @@
 package com.example.propertycostumer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
+
+    EditText userEmail, userPassword;
+    Button login;
+
+    TextView signup;
+    FirebaseAuth mAuth;
+    ProgressDialog progressDialog;
+    DatabaseReference databaseReference;
+    ValueEventListener valueEventListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loging in");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        userEmail = findViewById(R.id.emailAddress);
+        userPassword = findViewById(R.id.pass);
+
+        signup= findViewById(R.id.Signingup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Login.this,SignUp.class);
+                startActivity(intent);
+            }
+        });
+
+        login = findViewById(R.id.loginbtn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressDialog.show();
+
+                final String email = userEmail.getText().toString();
+                final String pswrd = userPassword.getText().toString();
+
+                if (!email.isEmpty() && !pswrd.isEmpty()) {
+                    databaseReference = FirebaseDatabase.getInstance().getReference().child("Costumers");
+
+                    valueEventListener = new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                            Log.e("exception", "try check 4");
+
+                            String logging = "";
+
+                            for (DataSnapshot data : dataSnapshot.getChildren()) {
+                                try {
+                                    Log.e("exception", "try check");
+
+                                    User model = data.getValue(User.class);
+
+                                    if (model != null && model.getEmail().equals(email) && model.getPassword().equals(pswrd)) {
+
+                                        Log.e("exception", "try check 2");
+                                        logging = "done";
+
+                                        progressDialog.dismiss();
+                                        Intent intent = new Intent(Login.this, VerifyPhoneActivity.class);
+                                        intent.putExtra("number", model.getNumber());
+                                        startActivity(intent);
+                                        finish();
+
+                                        return;
+
+                                    }
+
+
+                                } catch (Exception e) {
+                                    Log.e("exception", e.getLocalizedMessage());
+                                    progressDialog.dismiss();
+                                    Toast.makeText(Login.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+
+
+                                }
+                            }
+
+                            if (!logging.equals("done")) {
+                                progressDialog.dismiss();
+                                Toast.makeText(Login.this, "Enter Valid Email and Password", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            Log.e("Exception", databaseError.getMessage());
+                            progressDialog.dismiss();
+                            Toast.makeText(Login.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+
+
+                        }
+                    };
+
+
+                    databaseReference.addValueEventListener(valueEventListener);
+
+                } else {
+                    progressDialog.dismiss();
+
+                    Toast.makeText(Login.this, "Enter Email and Password", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/propertycostumer/SignUp.java
+++ app/src/main/java/com/example/propertycostumer/SignUp.java
@@ -1,14 +1,110 @@
 package com.example.propertycostumer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUp extends AppCompatActivity {
+
+    EditText entername, enteremail, enterpassword, enternumber;
+    Button signup;
+
+    FirebaseAuth mAuth;
+    DatabaseReference databaseReference;
+    ProgressDialog progressDialog;
+
+    AwesomeValidation awesomeValidation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Registering");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        entername = findViewById(R.id.name);
+        enteremail = findViewById(R.id.email);
+        enternumber = findViewById(R.id.number);
+        enterpassword = findViewById(R.id.password);
+
+        mAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Costumers");
+
+        String regexPassword = "(?=.*[a-z]).{6,}";
+        awesomeValidation.addValidation(SignUp.this, R.id.name, "[a-zA-Z\\s]+", R.string.name);
+        awesomeValidation.addValidation(SignUp.this, R.id.email, android.util.Patterns.EMAIL_ADDRESS, R.string.email);
+        awesomeValidation.addValidation(SignUp.this, R.id.password, regexPassword, R.string.password);
+
+        signup = findViewById(R.id.signupbtn);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (awesomeValidation.validate()) {
+
+                    progressDialog.show();
+                    mAuth.createUserWithEmailAndPassword(enteremail.getText().toString(), enterpassword.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        Log.e("successful", user.getUid());
+
+                                        User users = new User(entername.getText().toString(),
+                                                enteremail.getText().toString(),
+                                                enternumber.getText().toString(),
+                                                enterpassword.getText().toString());
+
+                                        databaseReference.child(user.getUid()).setValue(users).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    progressDialog.dismiss();
+                                                    Intent intent = new Intent(SignUp.this, VerifyPhoneActivity.class);
+                                                    intent.putExtra("number", enternumber.getText().toString());
+                                                    startActivity(intent);
+                                                    progressDialog.dismiss();
+                                                    finish();
+                                                } else {
+                                                    progressDialog.dismiss();
+                                                    Log.e("Exception", task.getException().getMessage());
+                                                }
+                                            }
+                                        });
+                                    } else {
+                                        Log.e("unsuccessful", task.getException().getMessage());
+                                        progressDialog.dismiss();
+                                    }
+
+                                }
+                            });
+
+
+                }
+            }
+        });
+
     }
 }
